98115f01abd96cf8312181d3b84346d5aff64619
Charles Labourier <charles@clabouri.dev>
2025-12-26 23:54:01 +0100

    fix(asgi): Add contaner wiring to asgi

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,11 +1,17 @@
 import os
 
 from app.logging import init_logging
+from app.presentation.mcp.container import Container
+from app.settings import Settings
 
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = os.environ.get("GUNICORN_WORKERS", 3)
 bind = "0.0.0.0:8000"
 
 logger_class = "app.logging.gunicorn.GunicornLogger"
+
+container = Container()
+container.config.from_pydantic(Settings())
+container.wire(packages=["app.presentation.mcp"])
 
 init_logging()
